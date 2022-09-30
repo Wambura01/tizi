3b9700afa5467f72c28faf25327d9d35ebfc33fb
brian-wambura <brian.partner@space44.com>
2022-09-30 09:31:03 +0300

    Finished hero and programs sections.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import "./App.css";
 import Hero from "./components/Hero/hero";
+import Programs from "./components/Programs/programs";
+
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Programs />
     </div>
   );
 }
