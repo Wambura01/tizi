4bfc55dc6f32c0c95474914c881c2fd6334a6d4a
brian-wambura <brian.partner@space44.com>
2022-10-27 06:18:46 +0300

    Made navigation responsive and fixed some box-model issues.

--- src/components/Plans/plans.jsx
+++ src/components/Plans/plans.jsx
@@ -8,7 +8,7 @@ import "./plans.css";
 
 function Plans() {
   return (
-    <div className="plans-container">
+    <div className="plans-container" id="plans">
       <div className="blur plans-blur-1"></div>
       <div className="blur plans-blur-2"></div>
       <div className="programs-header">
